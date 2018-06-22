a09d8ccc37b0a1c16e2d8c10711989291c09af06
Piper Merriam <pipermerriam@gmail.com>
2018-06-22 15:53:21 -0600

    dirty

--- p2p/tools/network.py
+++ p2p/tools/network.py
@@ -1,5 +1,4 @@
 import asyncio
-from asyncio.base_events import Server
 import logging
 from typing import (
     Dict,
@@ -10,35 +9,7 @@ from typing import (
 )
 
 
-class Address(NamedTuple):
-    transport: str
-    ip: str
-    port: int
-
-
-logger = logging.getLogger('p2p.testing.network')
-
-
-class MockServer(Server):
-    """
-    Mock `asyncio.Server` object.
-    """
-    def __init__(self, client_connected_cb, address, network):
-        self.client_connected_cb = client_connected_cb
-        self.address = address
-        self.network = network
-
-    def __repr__(self):
-        return '<%s %s:%s:%s>' % (self.__class__.__name__, self.address.transport, self.address.ip, self.address.port)
-
-    def close(self):
-        pass
-
-    async def wait_closed(self):
-        return
-
-
-class _MemoryTransport(asyncio.Transport):
+class MemoryTransport(asyncio.Transport):
     """Direct connection between a StreamWriter and StreamReader."""
 
     def __init__(self, reader: asyncio.StreamReader) -> None:
@@ -66,7 +37,7 @@ class _MemoryTransport(asyncio.Transport):
         self.write_eof()
 
 
-def mempipe(ip, port) -> Tuple[asyncio.StreamReader, asyncio.StreamWriter]:
+def mempipe(host, port) -> Tuple[asyncio.StreamReader, asyncio.StreamWriter]:
     """In-memory pipe, returns a ``(reader, writer)`` pair.
 
     .. versionadded:: 0.1
@@ -74,8 +45,11 @@ def mempipe(ip, port) -> Tuple[asyncio.StreamReader, asyncio.StreamWriter]:
     """
 
     reader = asyncio.StreamReader()
+    # TODO: attach host to memory transport
+    transport = MemoryTransport(reader),
+
     writer = asyncio.StreamWriter(
-        transport=_MemoryTransport(reader),
+        transport=transport,
         protocol=asyncio.StreamReaderProtocol(reader),
         reader=reader,
         loop=asyncio.get_event_loop(),
@@ -87,27 +61,57 @@ def get_connected_readers(server, client):
     server_reader, client_writer = mempipe()
     client_reader, server_writer = mempipe()
 
-    server_reader.__id = 'lr'
-    server_writer.__id = 'lw'
-    client_reader.__id = 'rr'
-    client_writer.__id = 'rw'
-
     return (
         server_reader, server_writer,
         client_reader, client_writer,
     )
 
 
+class Address(NamedTuple):
+    transport: str
+    ip: str
+    port: int
+
+
+logger = logging.getLogger('p2p.testing.network')
+
+
+class Server:
+    """
+    Mock version of `asyncio.Server` object.
+    """
+    def __init__(self, client_connected_cb, address, network):
+        self.client_connected_cb = client_connected_cb
+        self.address = address
+        self.network = network
+
+    def __repr__(self):
+        return '<%s %s:%s:%s>' % (self.__class__.__name__, self.address.transport, self.address.ip, self.address.port)
+
+    def close(self):
+        pass
+
+    async def wait_closed(self):
+        return
+
+
 # TODO: Need a `Router` and a `Network`.  Network is aware of IP address and Router
 
 
-class MockNetwork:
-    servers: Dict[Address, MockServer] = None
-    connections: Set[Tuple[Address, Address]] = None
+class Network:
+    servers: Dict[Address, Server] = None
+    connections: Set[Address] = None
 
-    def __init__(self):
+    def __init__(self, host, router):
+        self.host
         self.servers = {}
         self.connections = set()
+
+    def get_server(self, address):
+        try:
+            return self.servers[address]
+        except KeyError:
+            raise Exception("No server running at {0}".format(address)
 
     async def start_server(self, client_connected_cb, host=None, port=None, *, loop=None, limit=None, **kwds) -> Server:
         address = Address('tcp', host, port)
@@ -116,7 +120,7 @@ class MockNetwork:
         self.servers[address] = server
         return server
 
-    async def open_connection(self, host=None, port=None, *, loop=None, limit=None, **kwds) -> Tuple[asyncio.StreamReader, asyncio.StreamWriter]:
+    async def open_connection(self, port=None, *, loop=None, limit=None, **kwds) -> Tuple[asyncio.StreamReader, asyncio.StreamWriter]:
         to_address = Address('tcp', host, port)
 
         if to_address not in self.servers:
@@ -128,17 +132,38 @@ class MockNetwork:
         if (to_address, from_address) in self.connections:
             raise Exception('already connected')
 
-        self.connections.add((to_address, from_address))
+        self.connections.add(to_address)
 
         server_reader, server_writer, client_reader, client_writer = get_connected_readers(
-            from_,
-
+            from_host=self.host,
+            to_address=to_address,
         )
 
         logger.info('IN OPEN_CONNECTION')
         asyncio.ensure_future(server.client_connected_cb(server_reader, server_writer))
         logger.info('RETURNING OPEN_CONNECTION')
         return client_reader, client_writer
+
+
+class Router:
+    networks: Dict[str, Network]
+
+    def create_network(self, host):
+        if host in self.networks:
+            raise Exception("network already exists")``
+        network = Network(host, self)
+        self.networks[host] = network
+        return network
+
+    def get_network(self, host):
+        try:
+            return self.network[host]
+        except KeyError:
+            raise Exception("unknown network")
+
+    async def open_connection(self, host=None, port=None, *, loop=None, limit=None, **kwds) -> Tuple[asyncio.StreamReader, asyncio.StreamWriter]:
+        network = self.get_network(host)
+        return network.open_connection(port)
 
 
 mock_network = MockNetwork()
